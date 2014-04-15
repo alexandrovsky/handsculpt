21ebf0a34c27a604a0261e34fc79dedab6d879af
alexandrovsky <alexandrovsky.dmitry@gmail.com>
2014-04-15 16:11:47 +0200

    add

---
+++ Assets/Scripts/SculptMesh/SculptMesh.cs
@@ -0,0 +1,606 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Sculpt{
+// add this script to the target object
+
+		public class SculptMesh : MonoBehaviour {
+
+		public List<Triangle> triangles;
+		public List<Vertex> vertices;
+
+		public Vector3[] vertexArray;
+		public Vector3[] normalArray;
+		public Color[] colorArray;
+
+		int[] indexArray; // triangle ideces
+		Octree octree;
+
+		MeshFilter meshFilter;
+		Mesh mesh;
+
+
+		List<int> iTrisCandidates; // candidates for picking;
+		List<int> iTrisInCells;
+		List<Octree> leavesUpdate; // leafes for mesh update
+
+
+		int pickedTriangle = -1;
+		public Vector3 intersectionPoint = Vector3.zero;
+
+		List<int> pickedVertices;
+
+		public float worldRadiusSqr = 0.0f;
+
+		public int debugTrisInOctreeCount;
+		public bool drawDebug = false;
+		void OnDrawGizmos()
+		{
+			if(!drawDebug)return;
+
+//			debugTrisInOctreeCount = 0;
+//			drawOctreeTriangles(this.octree);
+//			Debug.Log("tris in octree "+ debugTrisInOctreeCount);
+
+//			drawVertices();
+//			drawPickedVerices();
+//			drawSelectedAabbs();
+
+//			drawLeavesUpdate();
+//			drawITrisInCells();
+			drawITrisCandidates();
+			drawOctree(this.octree);
+//			drawTriangleAabbs();
+		}
+
+
+		void drawOctreeTriangles(Octree root){
+			if(root == null) return;
+
+			Gizmos.color = Color.green;
+//			Gizmos.DrawWireCube(root.aabbSplit.center, root.aabbSplit.size);
+//			Gizmos.color = Color.cyan;
+//			Gizmos.DrawWireCube(root.aabbLoose.center, root.aabbLoose.size);
+			debugTrisInOctreeCount += root.children.Count;
+			if(root.children.Count == 8){
+				foreach(Octree child in root.children){
+					drawOctreeTriangles(child);
+					//				Debug.DrawLine(root.aabbSplit.center, child.aabbSplit.center, Color.blue);
+				}
+			}else if(root.iTris.Count != 0){
+
+				Gizmos.color = Color.yellow;
+				Gizmos.DrawWireCube(root.aabbSplit.center, root.aabbSplit.size);
+
+
+				Gizmos.color = Color.green;
+				foreach(int iTri in root.iTris){
+					Triangle t = triangles[iTri];
+					Gizmos.DrawWireCube(t.aabb.center, t.aabb.size);
+//					Debug.DrawLine(root.aabbSplit.center, t.aabb.center, Color.red);
+				}
+			}
+
+		}
+
+
+		void drawVertices(){
+			if(vertices == null && vertices.Count == 0) return;
+
+			foreach(Vertex v in vertices){
+				if(v == null) return;
+				Vector3 vertex = transform.TransformPoint(vertexArray[v.id]);
+//				Debug.DrawLine(transform.position, vertex, Color.green);
+
+				foreach(int v_idx in v.ringVertices){
+					Vector3 ringVert = transform.TransformPoint(vertexArray[v_idx]);
+					Debug.DrawLine(ringVert, vertex, Color.green);
+				}
+//				for(int i = 0; i < v.tIndices.Count; i++){
+//					Triangle t = triangles[v.tIndices[i]];
+//					Debug.DrawLine(vertex, t.leaf.aabbSplit.center, Color.green);
+//				}
+
+			}
+		}
+
+		void drawPickedVerices(){
+			foreach(int v_idx in pickedVertices){
+				Vector3 v = transform.TransformPoint( vertexArray[v_idx] );
+				Debug.DrawLine(this.intersectionPoint, v, Color.blue);
+			}
+		}
+
+		void drawITrisInCells(){
+			Gizmos.color = Color.cyan;
+			if(iTrisInCells == null) return;
+			foreach(int iTris in iTrisInCells){
+				Triangle t = triangles[iTris];
+				Gizmos.DrawWireCube(t.aabb.center, t.aabb.size);
+			}
+			
+			//			if(pickedTriangle != -1){
+			//				Triangle t = triangles[pickedTriangle];
+			//				Gizmos.DrawWireCube(t.aabb.center, t.aabb.size);
+			//			}
+			
+		}
+
+		void drawITrisCandidates(){
+//			Gizmos.color = Color.cyan;
+//			foreach(int iTris in iTrisCandidates){
+//				Triangle t = triangles[iTris];
+//				Gizmos.DrawWireCube(t.aabb.center, t.aabb.size);
+//			}
+
+			if(pickedTriangle != -1){
+				Gizmos.color = Color.red;
+				Triangle t = triangles[pickedTriangle];
+				Gizmos.DrawWireCube(t.aabb.center, t.aabb.size);
+			}
+
+		}
+
+		void drawLeavesUpdate(){
+			if(leavesUpdate == null) return;
+			foreach(Octree node in leavesUpdate){
+				Gizmos.color = Color.red;
+				Gizmos.DrawWireCube(node.aabbSplit.center, node.aabbSplit.size);
+			}
+		}
+
+		void drawSelectedAabbs(){
+			foreach(Octree node in Octree.selectedNodes){
+				Gizmos.color = Color.yellow;
+				Gizmos.DrawWireCube(node.aabbSplit.center, node.aabbSplit.size);
+			}
+		}
+
+		void drawTriangleAabbs(){
+			Gizmos.color = Color.cyan;
+			foreach(Triangle t in triangles)
+			{
+	//				Triangle t = triangles[42];
+				Gizmos.DrawWireCube(t.aabb.center, t.aabb.size);
+//				Debug.DrawLine(t.aabb.center, t.leaf.aabbSplit.center, Color.red);
+
+//				int iVert1 = indexArray[t.id * 3 + 0];
+//				int iVert2 = indexArray[t.id * 3 + 1];
+//				int iVert3 = indexArray[t.id * 3 + 2];
+//				Vector3 v1 = transform.TransformPoint(vertexArray[iVert1]);
+//				Vector3 v2 = transform.TransformPoint(vertexArray[iVert2]);
+//				Vector3 v3 = transform.TransformPoint(vertexArray[iVert3]);
+//
+//				Debug.DrawLine(v1, t.aabb.center, Color.green);
+//				Debug.DrawLine(v2, t.aabb.center, Color.green);
+//				Debug.DrawLine(v3, t.aabb.center, Color.green);
+			}
+		}
+
+		void drawOctree(Octree root){
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawWireCube(root.aabbSplit.center, root.aabbSplit.size);
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawWireCube(root.aabbLoose.center, root.aabbLoose.size);
+			foreach(Octree child in root.children){
+				drawOctree(child);
+//				Debug.DrawLine(root.aabbSplit.center, child.aabbSplit.center, Color.blue);
+			}
+		}
+
+		// Use this for initialization
+		void Start () {
+
+			vertices = new List<Vertex>();
+			triangles = new List<Triangle>();
+			iTrisCandidates = new List<int>();
+			iTrisInCells = new List<int>();
+			leavesUpdate = new List<Octree>();
+			pickedVertices = new List<int>();
+			initMesh();
+		}
+
+		void initMesh(){
+
+	//			GameObject target = GameObject.Find("Target");
+			
+			meshFilter = GetComponent<MeshFilter>();
+			mesh = meshFilter.sharedMesh; 
+
+	//			MeshFilter mf = GetComponent<MeshFilter>();
+	//			Mesh mesh = mf.mesh;
+			
+			vertexArray = mesh.vertices;
+			normalArray = mesh.normals;
+			indexArray = mesh.triangles;
+			colorArray = mesh.colors;
+
+			Bounds aabb = new Bounds();
+
+			// create empty vertices:
+			for(int i = 0; i < vertexArray.Length; i++){
+				vertices.Add(new Vertex(i) );
+//				this.coumputeRingVertices(i);
+				Vector3 vertex = this.transform.TransformPoint(vertexArray[i]);
+				aabb.Encapsulate(vertex );
+			}
+
+
+
+
+			// compute neigbour triangles for a vertex:
+//			for(int triangleIndex = 0; triangleIndex < vertexArray.Length; triangleIndex++){
+//			for(int t_idx = 0; t_idx < triangles.Count; t_idx+=3){
+			int nbTriangles = indexArray.Length/3;
+			for(int i = 0; i < nbTriangles; i++){
+				int iV1 = indexArray[i * 3 + 0];
+				int iV2 = indexArray[i * 3 + 1];
+				int iV3 = indexArray[i * 3 + 2];
+				
+				Vertex v1 = vertices[iV1];
+				Vertex v2 = vertices[iV2];
+				Vertex v3 = vertices[iV3];
+
+				v1.tIndices.Add(i);
+				v2.tIndices.Add(i);
+				v3.tIndices.Add(i);
+
+			}
+
+
+
+			for(int i = 0; i < vertexArray.Length; i++){
+				coumputeRingVertices(i);
+			}
+
+
+//			int nbTriangles = indexArray.Length/3;
+
+			for(int i = 0; i < nbTriangles; i++){
+				Triangle t = new Triangle(i);
+
+				updateTriangleAabbAndNormal(t);
+				triangles.Add(t);
+			}
+
+			computeOctree(aabb);
+
+
+		}
+
+		void updateTriangleAabbAndNormal(Triangle t){
+			int iTri = t.id;
+			
+			int iVert1 = indexArray[3 * iTri + 0];
+			int iVert2 = indexArray[3 * iTri + 1];
+			int iVert3 = indexArray[3 * iTri + 2];
+			
+			Vector3 v1 = transform.TransformPoint(vertexArray[iVert1]);
+			Vector3 v2 = transform.TransformPoint(vertexArray[iVert2]);
+			Vector3 v3 = transform.TransformPoint(vertexArray[iVert3]);
+			
+			Geometry.triangleNormal(v1, v2, v3, out t.normal);
+			Geometry.triangleAabb(v1, v2, v3, out t.aabb);
+		}
+
+		void updateTrianglesAabbAndNormal(List<int> iTris){
+			for(int i = 0; i < iTris.Count; i++){
+				Triangle t = triangles[iTris[i]];
+				updateTriangleAabbAndNormal(t);
+
+			}
+		}
+
+		void computeOctree(Bounds aabbSplit){
+			this.octree = new Octree(null, Octree.maxDepth);
+			this.octree.aabbSplit.SetMinMax(aabbSplit.min, aabbSplit.max);
+
+			int nbTriangles = triangles.Count;
+			List<int> allTris = new List<int>();
+			for(int i = 0; i < nbTriangles; i++){
+				allTris.Add(i);
+			}
+
+			octree.build(this, allTris);
+		}
+
+  /**
+   * Update Octree
+   * For each triangle we check if its position inside the octree has changed
+   * if so... we mark this triangle and we remove it from its former cells
+   * We push back the marked triangles into the octree
+   */
+		public void updateOctree(List<int>iTris)
+		{
+			int nbTris = iTris.Count;
+			List<int> trisToMove = new List<int>();
+
+
+			Octree leaf;
+			List<int> trisLeaf;
+			for (int i = 0; i < nbTris; ++i) //recompute position inside the octree
+			{
+				Triangle t = triangles[iTris[i]];
+				leaf = t.leaf;
+//				trisToMove.Add(iTris[i]);
+//				leaf.iTris.Remove(t.id);
+				if ( !leaf.aabbSplit.Contains(t.aabb.center) )
+				{
+					trisToMove.Add(iTris[i]);
+					trisLeaf = leaf.iTris;
+					if(leaf.iTris.Contains(t.id) ){
+						leaf.iTris.Remove(t.id);
+//						t.leaf = null;
+					}
+				}
+				else if (!t.aabb.Intersects(leaf.aabbLoose)){
+					leaf.aabbLoose.Encapsulate(t.aabb);
+				}
+			}
+			int nbTrisToMove = trisToMove.Count;
+			for (int i = 0; i < nbTrisToMove; ++i) //add triangle to the octree
+			{
+				Triangle tri = triangles[trisToMove[i]];
+				if (!this.octree.aabbLoose.Intersects(tri.aabb)) //we reconstruct the whole octree, slow... but rare
+				{
+					Bounds aabb = new Bounds(octree.aabbSplit.center, octree.aabbSplit.size);
+
+					List<int> allTris = new List<int>();
+					var nbTriangles = triangles.Count;
+					for (i = 0; i < nbTriangles; ++i){
+						allTris.Add(i);
+					}
+					List<int> iVerts = getVerticesForTriangles(trisToMove);
+					for(int j = 0; j <iVerts.Count; j++){
+						Vector3 vertex = transform.TransformPoint(vertexArray[iVerts[j]]);
+						aabb.Encapsulate(vertex);
+					}
+
+					this.octree = new Octree(null, Octree.maxDepth);
+					this.octree.aabbSplit = aabb;
+					this.octree.build(this, allTris);
+					this.leavesUpdate.Clear();
+					break;
+				}
+				else
+				{
+					leaf = tri.leaf;
+					this.octree.addTriangle(tri);
+
+//					if (leaf == tri.leaf) // failed to insert tri in octree
+//					{
+//						Debug.Log("failed to insert triangle[" + tri.id + "]");
+//						trisLeaf = leaf.iTris;
+//						tri.posInLeaf = trisLeaf.Count;
+//						trisLeaf.Add(trisToMove[i]);
+//					}
+				}
+			}
+		}
+
+		public void pushMeshData(){
+			mesh.vertices = this.vertexArray;
+			mesh.normals = this.normalArray;
+			mesh.triangles = this.indexArray;
+			mesh.colors = this.colorArray;
+		}
+
+		public void updateMesh(List<int> iTris, List<int> iVerts){
+			updateTrianglesAabbAndNormal(iTris);
+			updateOctree(iTris);
+
+			pushMeshData();
+
+
+		}
+
+		public void coumputeRingVertices(int iVert){
+			Vertex v = vertices[iVert];
+			v.ringVertices.Clear();
+
+
+			for(int i = 0; i < v.tIndices.Count; i++){
+				int iTri = v.tIndices[i] * 3;
+				int iVert1 = indexArray[iTri + 0];
+				int iVert2 = indexArray[iTri + 1];
+				int iVert3 = indexArray[iTri + 2];
+
+				if(iVert != iVert1) v.ringVertices.Add(iVert1);
+				if(iVert != iVert2) v.ringVertices.Add(iVert2);
+				if(iVert != iVert3) v.ringVertices.Add(iVert3);
+			}
+		}
+
+		public void intersectRayMesh(Ray ray){
+			Octree.selectedNodes.Clear();
+	//		foreach(int iTri in iTrisCandidates){
+	//			
+	//			int iVert1 = indexArray[iTri + 0];
+	//			int iVert2 = indexArray[iTri + 1];
+	//			int iVert3 = indexArray[iTri + 2];
+	//			
+	//			colorArray[iVert1] = Color.white;
+	//			colorArray[iVert2] = Color.white;
+	//			colorArray[iVert3] = Color.white;
+	//		}
+
+			iTrisCandidates = octree.intersectRay(ray);
+
+			float minDist = Mathf.Infinity;
+			this.pickedTriangle = -1;
+			foreach(int iTri in iTrisCandidates){
+
+				Triangle t = triangles[iTri];
+				if( t.aabb.IntersectRay(ray) ){
+
+					if(Vector3.Distance(ray.origin, t.aabb.center) < minDist ){
+
+						int iVert1 = indexArray[3 * iTri + 0];
+						int iVert2 = indexArray[3 * iTri + 1];
+						int iVert3 = indexArray[3 * iTri + 2];
+						
+//						colorArray[iVert1] = Color.red;
+//						colorArray[iVert2] = Color.red;
+//						colorArray[iVert3] = Color.red;
+
+						Vector3 v1 = transform.TransformPoint( vertexArray[iVert1] );
+						Vector3 v2 = transform.TransformPoint( vertexArray[iVert2] );
+						Vector3 v3 = transform.TransformPoint( vertexArray[iVert3] );
+
+						float dist = Vector3.Distance(ray.origin, v1);
+						if(dist < minDist) {
+							minDist = dist;
+							this.pickedTriangle = iTri;
+							this.intersectionPoint = v1;
+						}
+
+						dist = Vector3.Distance(ray.origin, v2);
+						if(dist < minDist) {
+							minDist = dist;
+							this.pickedTriangle = iTri;
+							this.intersectionPoint = v2;
+						}
+
+						dist = Vector3.Distance(ray.origin, v3);
+						if(dist < minDist) {
+							minDist = dist;
+							this.pickedTriangle = iTri;
+							this.intersectionPoint = v3;
+						}
+					}
+
+				}
+				if(-1 != this.pickedTriangle){
+					this.worldRadiusSqr = 1.0f;
+				}else{
+					this.worldRadiusSqr = 0.0f;
+				}
+			}
+
+			mesh.colors = colorArray;
+		}
+
+
+		public List<int> pickVerticesInSphere(float radius){
+			this.leavesUpdate.Clear();
+			this.pickedVertices.Clear();
+
+			this.worldRadiusSqr = radius;
+			this.iTrisInCells = this.octree.intersectSphere(this.intersectionPoint, this.worldRadiusSqr, this.leavesUpdate);
+
+			List<int> iVerts = getVerticesForTriangles(this.iTrisInCells);
+			long vertexSculptMask = ++Vertex.SculptMask;
+			for(int i = 0; i < iVerts.Count; i++){
+				int idx = iVerts[i];
+
+				Vector3 v = transform.TransformPoint( vertexArray[idx] );
+				float dist = Vector3.Distance(this.intersectionPoint, v);
+				if(dist < this.worldRadiusSqr){
+					vertices[idx].sculptFlag = vertexSculptMask;
+					this.pickedVertices.Add(idx);
+
+//					colorArray[idx] = Color.red;
+
+				}
+			}
+			if (this.pickedVertices.Count == 0 && this.pickedTriangle != -1) //no vertices inside the brush radius (big triangle or small radius)
+			{
+
+				int j = this.pickedTriangle * 3;
+				this.pickedVertices.Add(this.indexArray[j]);
+				this.pickedVertices.Add(this.indexArray[j+1]);
+				this.pickedVertices.Add(this.indexArray[j+2]);
+				                       
+			}
+			return this.pickedVertices;
+		}
+
+		public List<int> getVerticesForTriangles(List<int> iTris){
+
+			long vertexTagMask = ++Vertex.TagMask;
+
+			List<int> iVerts = new List<int>();
+			for(int i = 0; i < iTris.Count; i++){
+				int triIdx = iTris[i] * 3;
+				int iVert1 = indexArray[triIdx + 0];
+				int iVert2 = indexArray[triIdx + 1];
+				int iVert3 = indexArray[triIdx + 2];
+
+				if (vertices[iVert1].tagFlag != vertexTagMask)
+				{
+					iVerts.Add(iVert1);
+					vertices[iVert1].tagFlag = vertexTagMask;
+				}
+				if (vertices[iVert2].tagFlag != vertexTagMask)
+				{
+					iVerts.Add(iVert2);
+					vertices[iVert2].tagFlag = vertexTagMask;
+				}
+				if (vertices[iVert3].tagFlag != vertexTagMask)
+				{
+					iVerts.Add(iVert3);
+					vertices[iVert3].tagFlag = vertexTagMask;
+				}
+			}
+			return iVerts;
+		}
+
+		public List<int>getTrianglesFromVertices(List<int>iVerts)
+		{
+			long triangleTagMask = ++Triangle.tagMask;
+			List<int> iTris = new List<int>();
+			int nbVerts = iVerts.Count;
+
+			for (var i = 0; i < nbVerts; ++i)
+			{
+				List<int> ringTris = vertices[iVerts[i]].tIndices;
+				int nbTris = ringTris.Count;
+				for (var j = 0; j < nbTris; ++j)
+				{
+					var iTri = ringTris[j];
+					if (triangles[iTri].tagFlag != triangleTagMask)
+					{
+						iTris.Add(iTri);
+						triangles[iTri].tagFlag = triangleTagMask;
+					}
+				}
+			}
+			return iTris;
+		}
+
+		/** Compute average normal of a group of vertices with culling */
+		public Vector3 areaNormal(List<int>iVerts)
+		{
+			int nbVerts = iVerts.Count;
+			Vector3 aNormal = Vector3.zero;
+			
+			for (int i = 0; i < nbVerts; ++i)
+			{
+				Vector3 normal = transform.TransformPoint(normalArray[iVerts[i]]);
+//				Vector3 v = transform.TransformPoint(vertexArray[iVerts[i]]);
+//				Debug.DrawLine(v, v + normal* 0.001f, Color.cyan);
+
+               aNormal += normal.normalized;
+       		}
+       		return aNormal.normalized;
+       	}
+
+		/** Compute average normal of a group of vertices with culling */
+		public Vector3 areaCenter(List<int>iVerts)
+		{
+			int nbVerts = iVerts.Count;
+			Vector3 aCenter = Vector3.zero;
+			
+			for (int i = 0; i < nbVerts; ++i)
+			{
+
+				Vector3 v = transform.TransformPoint(vertexArray[iVerts[i]]);
+				//				Debug.DrawLine(v, v + normal* 0.001f, Color.cyan);
+				
+				aCenter += v;
+			}
+			return aCenter * (1.0f/nbVerts);
+		}
+
+	}
+}
---
+++ Assets/Scripts/SculptMesh/Sculpter.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Sculpt{
+	public enum Tool{
+		BRUSH,
+		DRAG,
+		SMOOTH
+	}
+	public class Sculpter : MonoBehaviour {
+
+		public static Color CLEAR = Color.white;
+		public static Color SELECTED = Color.yellow;
+
+		float radius = 1.0f;
+
+		float detailSubdivision = 0.75f; //maximal edge length before we subdivide it
+		float detailDecimation = 0.1f; //minimal edge length before we collapse it (dependent of detailSubdivision)
+
+		float d2Min = 0.0f; 			// uniform refinement of mesh (min edge length)
+		float d2Max = 0.0f; 			// uniform refinement of mesh (max edge length)
+		float d2Thickness = 0.5f; 	// distance between 2 vertices before split/merge
+		float d2Move = 0.0f; 		// max displacement of vertices per step
+
+		List<int> iVertsSelected = new List<int>();
+		List<int> iTrisSelected = new List<int>();
+			
+
+		public Tool tool;
+		Ray ray = new Ray();
+		SculptMesh sculptMesh;
+		void OnDrawGizmos(){
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(ray.origin, ray.origin + ray.direction * 100);
+
+		}
+
+		// Use this for initialization
+		void Start () {
+			sculptMesh = GetComponent<SculptMesh>();
+			tool = Tool.BRUSH;
+			Debug.Log("sculpter");
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+			for(int i = 0; i < sculptMesh.colorArray.Length; i++){
+				sculptMesh.colorArray[i] = CLEAR;
+			}
+
+			if( Input.GetKeyDown(KeyCode.H) )
+			{
+				sculptMesh.drawDebug = !sculptMesh.drawDebug;
+			}
+
+			if( Input.GetKeyDown(KeyCode.Plus) )
+			{
+				radius += 0.05f;
+			}
+			if( Input.GetKeyDown(KeyCode.Minus) )
+			{
+				radius -= 0.05f;
+				if(radius < 0) radius = 0.05f;
+			}
+
+//			if(Input.GetMouseButtonDown(0))
+			{
+				Camera camera = Camera.main;
+				Vector3 mousePos = Input.mousePosition;
+				ray = camera.ScreenPointToRay(mousePos);
+				sculptMesh.intersectRayMesh(ray);
+//				this.radius = 1.0f;
+
+
+				List<int> pickedVertices = sculptMesh.pickVerticesInSphere(this.radius);
+				Vector3 center = sculptMesh.intersectionPoint;
+				float intensity = 1.0f;
+//				if(Input.GetMouseButton(0) )
+				{
+					sculpt(camera.transform.forward, pickedVertices, 
+				       sculptMesh.intersectionPoint, sculptMesh.worldRadiusSqr, intensity,this.tool);
+				}
+			}
+			sculptMesh.pushMeshData();
+		}
+
+		public void setAdaptiveParameters(float radiusSquared)
+		{
+			this.d2Max = radiusSquared * (1.1f - this.detailSubdivision) * 0.2f;
+			this.d2Min = this.d2Max / 4.2025f;
+			this.d2Move = this.d2Min * 0.2375f;
+			this.d2Thickness = (4.0f * this.d2Move + this.d2Max / 3.0f) * 1.1f;
+		}
+		
+		
+		public void sculpt(Vector3 eyeDir, List<int> pickedVertices, Vector3 center, float radius, float intensity, Sculpt.Tool tool){
+
+			long vertexSculptMask = Vertex.SculptMask;
+
+			List<int> iVertsInRadius = new List<int>();
+			List<int> iVertsInFront = new List<int>();
+
+
+
+			this.iVertsSelected = pickedVertices;
+			this.iTrisSelected = sculptMesh.getTrianglesFromVertices(iVertsSelected);
+			int nbVertsSelected = pickedVertices.Count;
+
+//			setAdaptiveParameters(radius);
+
+			// topology here....
+
+
+			for(int i = 0; i < nbVertsSelected; i++){
+				int idx = iVertsSelected[i];
+
+
+				if(sculptMesh.vertices[idx].sculptFlag == vertexSculptMask){
+					iVertsInRadius.Add(idx);
+
+
+
+					Vector3 v = transform.TransformPoint(sculptMesh.vertexArray[idx]);
+					Vector3 n = transform.TransformPoint(sculptMesh.normalArray[idx]);
+					if(Vector3.Dot(eyeDir, n) <= 0.0f ){
+						iVertsInFront.Add(idx);
+//						Debug.DrawLine(sculptMesh.intersectionPoint, v, Color.red);
+					}else{
+//						Debug.DrawLine(sculptMesh.intersectionPoint, v, Color.cyan);
+					} 
+				}
+			}
+
+			if(iVertsInFront.Count > 0){
+				switch(tool){
+				case Tool.BRUSH:
+					brush(center, iVertsInRadius, iVertsInFront, radius, intensity);
+					break;
+				case Tool.DRAG:
+					break;
+				case Tool.SMOOTH:
+					break;
+				}
+			}
+			sculptMesh.updateMesh(iTrisSelected, iVertsSelected);
+		}
+
+		/** Brush stroke, move vertices along a direction computed by their averaging normals */
+		public void brush(Vector3 center, List<int> iVertsInRadius, List<int> iVertsFront, float radius, float intensity){
+			Vector3 aNormal = sculptMesh.areaNormal(iVertsFront);
+			Vector3 aCenter = sculptMesh.areaCenter(iVertsFront);
+//			Debug.DrawLine(sculptMesh.intersectionPoint, aCenter, Color.cyan);
+
+			float deformIntensityBrush = intensity * radius * 0.1f;
+			float deformIntensityFlatten = intensity * 0.3f;
+
+			int nbVerts = iVertsFront.Count;
+			for(int i = 0; i < nbVerts; i++){
+				int v_idx = iVertsFront[i];
+				Vector3 v = sculptMesh.transform.TransformPoint(sculptMesh.vertexArray[v_idx]);
+				Vector3 delta = v - center;
+
+				float distanceToPlane = (v.x - aCenter.x) * aNormal.x + (v.y - aCenter.y) * aNormal.y + (v.z - aCenter.z) * aNormal.z;
+				float dist = delta.magnitude/radius;
+				float fallOff = dist * dist;
+
+				fallOff = 3.0f * fallOff * fallOff - 4.0f * fallOff * dist + 1.0f;
+				fallOff = fallOff * (distanceToPlane * deformIntensityFlatten - deformIntensityBrush);
+
+				if(Input.GetMouseButton(0) ){
+					v -= aNormal * fallOff;
+				}
+
+				sculptMesh.vertexArray[v_idx] = sculptMesh.transform.InverseTransformPoint(v);
+				sculptMesh.colorArray[v_idx] = SELECTED;
+			}
+
+		}
+
+
+
+	}
+}
