45303b4628138f55de26f966632da0773282b905
alexandrovsky <alexandrovsky.dmitry@gmail.com>
2014-05-28 21:52:13 +0200

    Add FreeFormMenues

---
+++ Assets/Scripts/Tools/ToolDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+public class ToolDispatcher : MonoBehaviour {
+
+	// Use this for initialization
+	GameObject Widget;
+	HandController handController;
+	void Start () {
+		handController = (GameObject.Find("LeapManager") as GameObject).GetComponent(typeof(HandController)) as HandController;
+		Widget = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Widget.transform.position = handController.leftHand.palm.transform.position;
+
+	}
+
+
+	public void WidgetGuiButtonPressed(int id){
+		Debug.Log("WidgetButtonPressed" + id);
+	}
+}
