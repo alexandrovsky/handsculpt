db534e83295246ed6c52299ae73684c112d77afa
alexandrovsky <alexandrovsky.dmitry@gmail.com>
2014-06-06 21:52:32 +0200

    gui cleanup

--- Assets/Scripts/Gui.cs
+++ Assets/Scripts/Gui.cs
@@ -153,187 +153,165 @@ public class Gui : MonoBehaviour {
 
 	void OnGUI(){
 
-		if(m_DisplayGui)
-		{
 
-			GUILayout.BeginArea (new Rect (0, 20 , 200, 400));
-
-
-			if( GUILayout.Button("Object Export") ){
-				MeshFilter objMeshFilter = target.GetComponent<MeshFilter>();
-				ObjExporter.MeshToFile(objMeshFilter, objectExportFilepath);
-			}
-			objectExportFilepath = GUILayout.TextField(objectExportFilepath);
-
-			if( GUILayout.Button("Subdivide") ){
-//				GameObject target = GameObject.Find("Target");
-//				TargetObjectScript tos = target.GetComponent<TargetObjectScript>();
-//				tos.Subdivide();
-
-				sculptMesh.subdivideMesh();
-			}
-
-			if( GUILayout.Button("Reset") ){
-
-				sculptMesh.resetMesh();
-			}
-			{
-				GUILayout.TextField( "verts count: " + sculptMesh.vertices.Count);
-				GUILayout.TextField( "tris count: " + sculptMesh.triangles.Count);
-
-				GUILayout.TextField( "scale: " + target.transform.localScale);
-			}
-
-			GUILayout.BeginHorizontal();
-			if( GUILayout.Button("Undo \n [ctrl] + [z]") ){
-				toolDispatcher.undo();
-			}
-			if( GUILayout.Button("Redo \n [ctrl]  + [y]") ){
-				toolDispatcher.redo();
-			}
-			GUILayout.EndHorizontal();
-
-			sculptMesh.drawDebug = GUILayout.Toggle(sculptMesh.drawDebug, "Debug");
-
-			GUILayout.EndArea();
-
-
-
-
-			// set tools:
-
-			GUILayout.BeginArea(new Rect (Screen.width-200, 20 , 200, 600));
-
-			// mouse mode
-			{
-				mouseMode = GUILayout.Toggle(mouseMode, "mouse enabled");
-				sculpter.isEnabled = mouseMode;
-				GUILayout.Label("Current Tool left" + toolDispatcher.currentLeftTool );
-				GUILayout.Label("Current Tool right" + toolDispatcher.currentRightTool );
-				GUILayout.BeginVertical();{
-					GUILayout.TextField("intesity:" + toolDispatcher.intensity);
-					toolDispatcher.intensity = GUILayout.HorizontalSlider(toolDispatcher.intensity, 0.0f, 1.0f);
-				}GUILayout.EndVertical();
-				
-				
-				GUILayout.BeginVertical();{
-					GUILayout.TextField("radius: " + toolDispatcher.radius);
-					toolDispatcher.radius = GUILayout.HorizontalSlider(toolDispatcher.radius, 0.01f, 3.0f);
-				}GUILayout.EndVertical();
-
-				GUILayout.BeginHorizontal();{
-					toolDispatcher.symmetry = GUILayout.Toggle(toolDispatcher.symmetry, "[s]ymmetry");
-					toolDispatcher.inverted = GUILayout.Toggle(toolDispatcher.inverted, "[i]nverted");
-				}GUILayout.EndHorizontal();
-
-
-			}
-			GUILayout.Label("-----");
-			if(!mouseMode){
-
-				GUILayout.Label("Pointing Tool");
-				GUILayout.BeginHorizontal();
-				if( GUILayout.Button( "Left [1]") ){
-					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_PAINT, handController.leftHand);
-				}
-				if( GUILayout.Button( "Right [2]") ){
-					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_PAINT, handController.rightHand);
-				}
-				GUILayout.EndHorizontal();
-
-
-
-				GUILayout.Label("Shadow Tool");
-				GUILayout.BeginHorizontal();
-				if( GUILayout.Button( "Left [3]") ){
-					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_SMOOTH, handController.leftHand);
-				}
-				if( GUILayout.Button( "Right [4]") ){
-					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_SMOOTH, handController.rightHand);
-				}
-				GUILayout.EndHorizontal();
-
-				GUILayout.Label("Grab Tool");
-				GUILayout.BeginHorizontal();
-				if( GUILayout.Button( "Left [5]") ){
-					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_GRAB, handController.leftHand);
-				}
-				if( GUILayout.Button( "Right [6]") ){
-					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_GRAB, handController.rightHand);
-				}
-				GUILayout.EndHorizontal();
-
-
-//				GUILayout.Label("Pointing Assistent Tool");
-//				GUILayout.BeginHorizontal();
-//				if( GUILayout.Button( "Left") ){
-//					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_PAINT_ASSISTENT, handController.leftHand);
-//				}
-//				if( GUILayout.Button( "Right") ){
-//					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_PAINT_ASSISTENT, handController.rightHand);
-//				}
-//				GUILayout.EndHorizontal();
-
-
-//				GUILayout.Label("Dynamic Assistent Tool");
-//				GUILayout.BeginHorizontal();
-//				if( GUILayout.Button( "Left") ){
-//					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.DYNAMIC_SCECONDARY, handController.leftHand);
-//				}
-//				if( GUILayout.Button( "Right") ){
-//					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.DYNAMIC_SCECONDARY, handController.rightHand);
-//				}
-//				GUILayout.EndHorizontal();
-
-
-				GUILayout.Label("Naigation Grab");
-				GUILayout.BeginHorizontal();
-				if( GUILayout.Button( "Left [7]") ){
-					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_NAVIGATION_GRAB, handController.leftHand);
-				}
-				if( GUILayout.Button( "Right [8]") ){
-					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_NAVIGATION_GRAB, handController.rightHand);
-				}
-				GUILayout.EndHorizontal();
-
-
-
-
-
-
-			}else{
-				// set the tool with the mouse
-			}
-
-			//GameObject hands = GameObject.Find("Leap Hands");	
-			//LeapUnityHandController controller = hands.GetComponent<LeapUnityHandController>();
-
-//			if( GUILayout.Button( "appearance") ){
-//				GameObject hands = GameObject.Find("Leap Hands");
-//
-//			}
-
-
-			GUILayout.EndArea();
-
-			// hand status:
-			GUILayout.BeginArea(new Rect(Screen.width/2, Screen.height-100, 50, 50));{
-				if(handController.rightHand.lost){
-					GUILayout.Label(m_greyTexture);
-				}else{
-					GUILayout.Label(m_greenTexture);
-				}
-			}GUILayout.EndArea();
-
-			GUILayout.BeginArea(new Rect(Screen.width/2-50, Screen.height-100, 50, 50));
-
-			if(handController.leftHand.lost){
+		// hand status:
+		GUILayout.BeginArea(new Rect(Screen.width/2, 20, 50, 50));{
+			if(handController.rightHand.lost){
 				GUILayout.Label(m_greyTexture);
 			}else{
 				GUILayout.Label(m_greenTexture);
 			}
+		}GUILayout.EndArea();
+		
+		GUILayout.BeginArea(new Rect(Screen.width/2-50, 20, 50, 50));
+		
+		if(handController.leftHand.lost){
+			GUILayout.Label(m_greyTexture);
+		}else{
+			GUILayout.Label(m_greenTexture);
+		}
+		GUILayout.EndArea();
+		
+		
+		if(m_DisplayGui)
+		{
+
+			GUILayout.BeginArea (new Rect (0, 20 , 200, 400));
+			meshControlGui();
+			GUILayout.EndArea();
+
+			// set tools:
+			GUILayout.BeginArea(new Rect (Screen.width-200, 20 , 200, 600));
+
+			toolParamsGui();
+
+			GUILayout.Label("-----");
+
+			toolSelectionGui();
 			GUILayout.EndArea();
 		}
 	}
 
+	private void meshControlGui(){
+
+		if( GUILayout.Button("Object Export") ){
+			MeshFilter objMeshFilter = target.GetComponent<MeshFilter>();
+			ObjExporter.MeshToFile(objMeshFilter, objectExportFilepath);
+		}
+		objectExportFilepath = GUILayout.TextField(objectExportFilepath);
+		
+		if( GUILayout.Button("Subdivide") ){
+			sculptMesh.subdivideMesh();
+		}
+		
+		if( GUILayout.Button("Reset") ){
+			sculptMesh.resetMesh();
+		}
+		{
+			GUILayout.TextField( "verts count: " + sculptMesh.vertices.Count);
+			GUILayout.TextField( "tris count: " + sculptMesh.triangles.Count);
+			
+			GUILayout.TextField( "scale: " + target.transform.localScale);
+		}
+		
+		GUILayout.BeginHorizontal();
+		if( GUILayout.Button("Undo \n [ctrl] + [z]") ){
+			toolDispatcher.undo();
+		}
+		if( GUILayout.Button("Redo \n [ctrl]  + [y]") ){
+			toolDispatcher.redo();
+		}
+		GUILayout.EndHorizontal();
+		
+		sculptMesh.drawDebug = GUILayout.Toggle(sculptMesh.drawDebug, "Debug");
+	}
+	private void toolParamsGui(){
+		GUILayout.Label("Current Tool left" + toolDispatcher.currentLeftTool );
+		GUILayout.Label("Current Tool right" + toolDispatcher.currentRightTool );
+		GUILayout.BeginVertical();{
+			GUILayout.TextField("intesity:" + toolDispatcher.intensity);
+			toolDispatcher.intensity = GUILayout.HorizontalSlider(toolDispatcher.intensity, 0.0f, 1.0f);
+		}GUILayout.EndVertical();
+		
+		
+		GUILayout.BeginVertical();{
+			GUILayout.TextField("radius: " + toolDispatcher.radius);
+			toolDispatcher.radius = GUILayout.HorizontalSlider(toolDispatcher.radius, 0.01f, 3.0f);
+		}GUILayout.EndVertical();
+		
+		GUILayout.BeginHorizontal();{
+			toolDispatcher.symmetry = GUILayout.Toggle(toolDispatcher.symmetry, "[s]ymmetry");
+			toolDispatcher.inverted = GUILayout.Toggle(toolDispatcher.inverted, "[i]nverted");
+		}GUILayout.EndHorizontal();
+	}
+	private void toolSelectionGui(){
+
+		GUILayout.Label("Pointing Tool");
+		GUILayout.BeginHorizontal();
+		if( GUILayout.Button( "Left [1]") ){
+			toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_PAINT, handController.leftHand);
+		}
+		if( GUILayout.Button( "Right [2]") ){
+			toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_PAINT, handController.rightHand);
+		}
+		GUILayout.EndHorizontal();
+		
+		
+		
+		GUILayout.Label("Shadow Tool");
+		GUILayout.BeginHorizontal();
+		if( GUILayout.Button( "Left [3]") ){
+			toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_SMOOTH, handController.leftHand);
+		}
+		if( GUILayout.Button( "Right [4]") ){
+			toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_SMOOTH, handController.rightHand);
+		}
+		GUILayout.EndHorizontal();
+
+
+
+		GUILayout.Label("Grab Tool");
+		GUILayout.BeginHorizontal();
+		if( GUILayout.Button( "Left [5]") ){
+			toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_GRAB, handController.leftHand);
+		}
+		if( GUILayout.Button( "Right [6]") ){
+			toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_GRAB, handController.rightHand);
+		}
+		GUILayout.EndHorizontal();
+
+		
+		GUILayout.Label("Naigation Grab");
+		GUILayout.BeginHorizontal();
+		if( GUILayout.Button( "Left [7]") ){
+			toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_NAVIGATION_GRAB, handController.leftHand);
+		}
+		if( GUILayout.Button( "Right [8]") ){
+			toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_NAVIGATION_GRAB, handController.rightHand);
+		}
+		GUILayout.EndHorizontal();
+		
+		
+		//				GUILayout.Label("Pointing Assistent Tool");
+	//				GUILayout.BeginHorizontal();
+	//				if( GUILayout.Button( "Left") ){
+	//					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_PAINT_ASSISTENT, handController.leftHand);
+	//				}
+	//				if( GUILayout.Button( "Right") ){
+	//					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.TOOL_PAINT_ASSISTENT, handController.rightHand);
+	//				}
+	//				GUILayout.EndHorizontal();
+	
+	
+	//				GUILayout.Label("Dynamic Assistent Tool");
+	//				GUILayout.BeginHorizontal();
+	//				if( GUILayout.Button( "Left") ){
+	//					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.DYNAMIC_SCECONDARY, handController.leftHand);
+	//				}
+	//				if( GUILayout.Button( "Right") ){
+	//					toolDispatcher.SetToolForHand(MenuBehavior.ButtonAction.DYNAMIC_SCECONDARY, handController.rightHand);
+	//				}
+	//				GUILayout.EndHorizontal();
+	}
+	
 }
